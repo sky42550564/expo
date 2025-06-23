6952e17417127e3728a66683905da6e67e174b04
fangyunjiang <42550564@qq.com>
2025-06-23 23:10:36 +0800

    修正snippets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { Image, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 export default function HomeScreen() {
+  const list = [1, 2, 3];
   return (
-    <View style={_u(`_wf_100_red _fx_c`)}>
-      <Image source={_img('logo.png')} style={_u(`_s_100`)} resizeMode="cover"></Image>
+    <View style={_u(`_wf_100 _fx_ccc`)}>
+      <View style={_u(`_button_white_warning_error_335_42_r`)}><Text>确定</Text></View>
     </View>
   );
 }
