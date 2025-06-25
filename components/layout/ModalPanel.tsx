c3a99f1c87911584b6c6766ab07fd13ddf6d3e53
fangyunjiang <42550564@qq.com>
2025-06-25 13:16:27 +0800

    修正错误

--- components/layout/ModalPanel.tsx
+++ components/layout/ModalPanel.tsx
@@ -9,47 +9,59 @@ type Props = PropsWithChildren<{
   width?: number, // 宽度
   height?: number, // 高度
   position?: 'bottom' | 'middle' | 'top', // 位置： bottom|middle|top
-  noTitle?: boolean, // 不显示标题栏
   radius?: number, // 曲半径
+  noTitle?: boolean, // 不显示标题栏
+  noClose?: boolean, // 不显示关闭按钮
 }>;
 
 export default forwardRef(({
   children, // 子组件
-  title = '提示', // 标题
-  width, // 宽度
-  height, // 高度
-  position, // 位置： bottom|middle|top
-  noTitle, // 不显示标题栏
-  radius, // 曲半径
+  title, // 标题
+  width = 375, // 宽度
+  height = 300, // 高度
+  position = 'bottom', // 位置： bottom|middle|top
+  radius = 6, // 曲半径
+  noTitle = false, // 不显示标题栏
+  noClose = false, // 不显示关闭按钮
 }: Props, ref) => {
   const [visible, setVisible] = useState(false);
-  const show = (options?: any) => {
-    options?.title && (title = options.title);
-    options?.width && (width = options.width);
-    options?.height && (height = options.height);
-    options?.position && (position = options.position);
-    options?.noTitle !== undefined && (noTitle = options.noTitle);
-    options?.radius !== undefined && (radius = options.radius);
+  const [option, setOption] = useState({
+    title, // 标题
+    width, // 宽度
+    height, // 高度
+    position, // 位置： bottom|middle|top
+    radius, // 曲半径
+    noTitle, // 不显示标题栏
+    noClose, // 不显示关闭按钮
+  });
+  const show = (params?: any) => {
+    params?.title && (option.title = params.title);
+    params?.width && (option.width = params.width);
+    params?.height && (option.height = params.height);
+    params?.position && (option.position = params.position);
+    params?.radius !== undefined && (option.radius = params.radius);
+    params?.noTitle !== undefined && (option.noTitle = params.noTitle);
+    params?.noClose !== undefined && (option.noClose = params.noClose);
     setVisible(true);
   }
   const close = () => {
     setVisible(false);
   }
-
   useImperativeHandle(ref, () => ({ show, close })); // 暴露函数组件内部方法
+
   return (
     <Modal animationType="fade" transparent statusBarTranslucent visible={visible}>
-      <View style={_u(`_full _bc_#00000050`, position === 'middle' && `_fx_rc`)}>
+      <View style={_u(`_full _bc_#00000050`, option.position === 'middle' && `_fx_rc`)}>
         {/* 边框 */}
-        <View style={_u(`_fx_c _of_hidden _por _p_10 _s_${width}_${height}_white`, position === 'bottom' ? `_pof_b0_l0 _brt_${radius}` : position === 'top' ? `_poa_t0_l0 _brb_${radius}` : `_br_${radius}`)}>
-          {/* 标题栏*/}
+        <View style={_u(`_fx_c _of_hidden _por _p_10 _s_${option.width}_${option.height}_white`, option.position === 'bottom' ? `_pof_b0_l0 _brt_${option.radius}` : option.position === 'top' ? `_poa_t0_l0 _brb_${option.radius}` : `_br_${option.radius}`)}>
+          {/* 标题栏 */}
           {
-            !noTitle &&
+            !option.noTitle &&
             <View style={_u(`_wf_30 _fx_rcc _pb_4 _bob_f8f8f8`)}>
-              {title && <View style={_u(`_fs_14_bold`)}>{title}</View> || <View></View>}
-              <View style={_u(`_poa_r10_t0`)}>
-                <Icon icon="close-circle-outline"></Icon>
-              </View>
+              {/* 标题 */}
+              {option.title && <View style={_u(`_fs_14_#a3a3a3_bold`)}>{option.title}</View> || <View></View>}
+              {/* 关闭按钮 */}
+              {!option.noClose && <View style={_u(`_poa_r10_t0`)}><Icon icon="close-circle-outline" color="#a3a3a3" onPress={close}></Icon></View>}
             </View>
           }
           {/* 内容 */}
