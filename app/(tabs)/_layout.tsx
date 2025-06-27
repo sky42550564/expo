a268a83967ac8e0ba56c2dd2fdf5eda877cf5026
fangyunjiang <42550564@qq.com>
2025-06-27 18:06:54 +0800

    修正相机

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,15 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform, View, Text } from 'react-native';
+import { Platform, View, Text, TouchableOpacity } from 'react-native';
+import CustomTabBar from '@/components/CustomTabBar';
 
 export default function TabLayout() {
   return (
     <Tabs
+      // tabBar={(props) => <CustomTabBar {...props} />} // 自定义的组件
       // https://reactnavigation.org/docs/bottom-tab-navigator/
       screenOptions={{
+        tabBarButton: (props) => <TouchableOpacity {..._.omit(props, (o: any) => o == null)} />, // 禁用所有反馈,
         tabBarPosition: 'bottom', // tabbar的位置
         tabBarActiveTintColor: 'blue', // 高亮颜色
         tabBarInactiveTintColor: 'gray', // 失活颜色
@@ -36,13 +39,20 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <Icon icon="checkmark-circle" color={color}></Icon>,
         }}
       />
+      {/* <Tabs.Screen
+        name="camera"
+        options={{
+          title: '相机',
+          tabBarIcon: ({ color }) => <Icon icon="scan-circle-outline" color={color}></Icon>,
+        }}
+      /> */}
       <Tabs.Screen
         name="camera"
         options={{
           title: '',
           tabBarIcon: ({ color }) => <Icon icon="scan-circle-outline" size={80} color={color}></Icon>,
           tabBarItemStyle: _u('_por'), // item的样式
-          tabBarIconStyle: _u(`_poa_t-20 _r_80_white _bo`), // icon的样式
+          tabBarIconStyle: _u(`_poa_t-32_l-4 _r_80_white _bo`), // icon的样式
         }}
       />
       <Tabs.Screen
